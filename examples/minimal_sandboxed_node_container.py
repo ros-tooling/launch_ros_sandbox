d4543cd05d63719f99c03e81137321ea1113c582
Zachary Michaels <zacmicha@amazon.com>
2019-08-26 10:38:53 -0700

    Add copyright, license and tests for checking copyright

--- examples/minimal_sandboxed_node_container.py
+++ examples/minimal_sandboxed_node_container.py
@@ -1,13 +1,26 @@
 #!/usr/bin/env python3
 
-# Copyright FIXME
+# Copyright 2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 
 """
 Minimal example for using SandboxedNodeContainer.
 
-This example runs a ROS 2 node in a sandboxed environment by invoking SandboxedNodeContainer action.
-SandboxedNodeContainer delegates the launch parameters to an instance of launch_ros that is running in
-a sandboxed environment using the requested sandboxing policy.
+This example runs a ROS 2 node in a sandboxed environment by invoking SandboxedNodeContainer
+action. SandboxedNodeContainer delegates the launch parameters to an instance of launch_ros that is
+running in a sandboxed environment using the requested sandboxing policy.
 """
 
 import sys
@@ -20,8 +33,8 @@ def generate_launch_description():
     """
     Create launch description for starting SandboxedNodeContainer.
 
-    Two nodes are loaded into the sandboxed container: talker and listener. No operation is performed since
-    no sandboxing policy was defined.
+    Two nodes are loaded into the sandboxed container: talker and listener. No operation is
+    performed since no sandboxing policy was defined.
     """
     ld = launch.LaunchDescription()
 
--- test/test_copyright.py
+++ test/test_copyright.py
@@ -1,4 +1,4 @@
-# Copyright 2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+# Copyright 2017 Open Source Robotics Foundation, Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,24 +12,12 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+from ament_copyright.main import main
+import pytest
 
-"""Package setup for launch_ros_sandbox."""
 
-from setuptools import find_packages
-from setuptools import setup
-
-setup(
-    name='launch_ros_sandbox',
-    version='0.1.0',
-    packages=find_packages(exclude=['test']),
-    install_requires=[
-        'setuptools',
-        'launch',
-    ],
-    zip_safe=True,
-    description='Sandbox extension to ROS 2 Launch.',
-    license='Apache License, Version 2.0',
-    tests_require=[
-        'pytest',
-    ],
-)
+@pytest.mark.copyright
+@pytest.mark.linter
+def test_copyright():
+    rc = main(argv=['.', 'test'])
+    assert rc == 0, 'Found errors'
