f7dbfe7ef00741ab3737e0365a3ab75c0623aa63
Zachary Michaels <zmichaels11@gmail.com>
2019-09-12 16:03:01 -0700

    Update test_docker_policy.sh
    * Use SIGINT to terminate the launch script.
    * Script now waits until docker container is running
    
    cr https://code.amazon.com/reviews/CR-13151715

--- test/test_docker_policy.sh
+++ test/test_docker_policy.sh
@@ -1,16 +1,43 @@
 #!/bin/bash
 
-# FIXME: launch script should hang until all subprocesses complete. This is currently not the case for DockerPolicy
-# due to the signal listeners being implemented yet for DockerPolicy.
-# Once this is implemented, the following command should be ran as its own process. The cleanup code should also
-# be changed to use `kill` instead of `docker stop`. The example could also drop the dependency of knowing the
-# container name.
-./examples/minimal_sandbox_docker.launch.py
-docker ps
+# pull the image now so that we don't have to guess/query when its done in the script
+docker pull osrf/ros:dashing-desktop
 
+# run the example; loads listener in Docker
+./examples/minimal_sandbox_docker.launch.py&
+task_id=$!
+
+echo "TaskID: $task_id"
+
+echo "Checking if sandboxed-listener-node is running..."
+docker inspect -f {{.State.Running}} sandboxed-listener-node
+is_running=$?
+
+# Wait until the docker container is running. docker inspect will return 0 when it does.
+while [[ $is_running -ne 0 ]]
+do
+  sleep 1
+  docker inspect -f {{.State.Running}} sandboxed-listener-node
+  is_running=$?
+done
+
+# Run listener in the container spun up by DockerPolicy for 5 seconds
 echo "Executing listener in Docker container..."
 timeout 5 docker exec -t sandboxed-listener-node /ros_entrypoint.sh ros2 run demo_nodes_cpp listener
 echo "Stopping Docker container..."
 
-# FIXME: this cleanup line should be replaced with a kill signal once SIGINT is handled by DockerPolicy.
-docker stop sandboxed-listener-node
+# Note: these sleep commands are here just so the following command executes after stdout appears on
+# the terminal.
+kill -INT $task_id
+sleep 2
+kill $task_id
+sleep 2
+
+echo "Checking if sandboxed-listener-node is running..."
+docker inspect -f {{.State.Running}} sandboxed-listener-node
+
+# Check the exit code of docker inspect. 0 is returned only if it is running.
+# This check will set the exit code to 0 only if the exit code is not 0.
+[[ $? -ne 0 ]]
+
+exit $?
