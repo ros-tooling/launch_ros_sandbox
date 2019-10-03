9ccc4d056e7923108f1c5df33d575893ac9b0105
Thomas Moulard <tmoulard@amazon.com>
2019-10-02 17:52:21 -0700

    Deduplicate index.rst and README.md
    Instead of copying/pasting README content in index.rst,
    directly include the file in the sphinx doc generation.
    
    Signed-off-by: Thomas Moulard <tmoulard@amazon.com>

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -32,13 +32,14 @@ sys.path.insert(0, os.path.abspath('../..'))
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'sphinx.ext.autodoc',
-    'sphinx.ext.doctest',
-    'sphinx.ext.todo',
-    'sphinx.ext.coverage',
-    'sphinx.ext.viewcode',
-    'sphinx.ext.githubpages',
     'recommonmark',
+    'sphinx.ext.autodoc',
+    'sphinx.ext.coverage',
+    'sphinx.ext.doctest',
+    'sphinx.ext.githubpages',
+    'sphinx.ext.todo',
+    'sphinx.ext.viewcode',
+    'sphinx_markdown_tables',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
